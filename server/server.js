a656579f41960906f7eeececd5359e8115a314e4
kevin <kevinkolyakov@gmail.com>
2025-07-22 20:06:48 -0400

    update server

--- server/server.js
+++ server/server.js
@@ -281,8 +281,10 @@ io.on('connection', socket => {
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+const indexPath = path.join(__dirname, '../client/build/index.html');
+
+app.get('/*', (req, res) => {
+  res.sendFile(indexPath);
 });
 
 const PORT = process.env.PORT || 3001;
